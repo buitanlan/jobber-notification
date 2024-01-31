006ec5e2cd144f6a0ed8c202274d1b26a4601498
Bùi Tấn Lân <tanlanhxmus@gmail.com>
2024-01-31 19:51:01 +0700

    feat(): create email client

---
+++ src/helper.ts
@@ -0,0 +1,51 @@
+import { IEmailLocals, winstonLogger } from '@buitanlan/jobber-shared';
+import { config } from '@notification/config';
+import nodemailer from 'nodemailer';
+import Email from 'email-templates';
+import path from 'path';
+
+const log = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'mailTransporterHelper', 'debug');
+
+export async function emailTemplate(template: string, receiver: string, locals: IEmailLocals) {
+  try {
+    const smtpTransport = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: config.SENDER_EMAIL,
+        pass: config.SENDER_PASSWORD
+      }
+    });
+
+    const email = new Email({
+      message: {
+        from: `Jobber App ${config.SENDER_EMAIL}`
+      },
+      send: true,
+      preview: false,
+      transport: smtpTransport,
+      views: {
+        options: {
+          extension: 'ejs'
+        }
+      },
+      juice: true,
+      juiceResources: {
+        preserveImportant: true,
+        webResources: {
+          relativeTo: path.join(__dirname, '../build')
+        }
+      }
+    });
+
+    await email.send({
+      template: path.join(__dirname, `..`,`/src/emails/`, template),
+      message: {
+        to: receiver
+      },
+      locals
+    })
+  } catch (error) {
+    log.log('error', 'NotificationService EmailTransport sendEmail() method:', error);
+  }
+}
--- src/queues/email.transport.ts
+++ src/queues/email.transport.ts
@@ -1,12 +1,13 @@
 import { IEmailLocals, winstonLogger } from '@buitanlan/jobber-shared';
 import { config } from '@notification/config';
+import { emailTemplate } from '@notification/helper';
 
 const log = winstonLogger(`${config.ELASTIC_SEARCH_URL}}`, 'emailTransport', 'debug');
 
 export async function sendEmail(template: string, receiverEmail: string, locals: IEmailLocals) {
   try {
+    await emailTemplate( template, receiverEmail, locals );
     log.info('Sending email successfully');
-
   } catch (error) {
     log.log('error', 'NotificationService EmailTransport sendEmail() method:', error);
   }
