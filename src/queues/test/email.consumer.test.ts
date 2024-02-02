add52fc2e0d98ca07e6a06105d17cec65059ba5a
BÙI TẤN LÂN <tanlanhcmus@gmail.com>
2024-02-02 19:24:47 +0700

    feat(): complete jest

--- src/queues/test/email.consumer.test.ts
+++ src/queues/test/email.consumer.test.ts
@@ -1,7 +1,7 @@
 import * as connection from '@notifications/queues/connection';
 
 import amqp from 'amqplib';
-import { consumeAuthEmailMessages } from '@notifications/queues/email.consumer';
+import { consumeAuthEmailMessages, consumeOrderEmailMessages } from '@notifications/queues/email.consumer';
 
 jest.mock('@notifications/queues/connection');
 jest.mock('amqplib');
@@ -39,6 +39,33 @@ describe('Email Consumer ', () => {
         'auth-email-queue',
         'jobber-email-notification',
         'auth-email',
+      );
+    });
+  });
+
+  describe('consumeOrderEmailMessages method', () => {
+    it('should be called', async () => {
+      const channel = {
+        assertExchange: jest.fn(),
+        publish: jest.fn(),
+        assertQueue: jest.fn(),
+        bindQueue: jest.fn(),
+        consume: jest.fn(),
+      };
+      jest.spyOn(channel, 'assertExchange');
+      jest
+        .spyOn(channel, 'assertQueue')
+        .mockReturnValue({ queue: 'order-email-queue', messageCount: 0, consumerCount: 0 });
+      jest.spyOn(connection, 'createConnection').mockReturnValue(channel as never);
+      const connectionChannel: amqp.Channel | undefined = await connection.createConnection();
+      await consumeOrderEmailMessages(connectionChannel!);
+      expect(connectionChannel!.assertExchange).toHaveBeenCalledWith('jobber-order-notification', 'direct');
+      expect(connectionChannel!.assertQueue).toHaveBeenCalledTimes(1);
+      expect(connectionChannel!.consume).toHaveBeenCalledTimes(1);
+      expect(connectionChannel!.bindQueue).toHaveBeenCalledWith(
+        'order-email-queue',
+        'jobber-order-notification',
+        'order-email',
       );
     });
   });
